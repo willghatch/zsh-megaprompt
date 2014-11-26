c8e1f18cefde6f8e224e2866f8e6881e672687ef
William G Hatch <willghatch@gmail.com>
2014-11-25 19:34:48 -0700

    change some convention, use λ, more customizable

--- zsh-megaprompt.plugin.zsh
+++ zsh-megaprompt.plugin.zsh
@@ -6,15 +6,19 @@ MEGAPROMPT_STYLES[time]="%b%F{cyan}"
 MEGAPROMPT_STYLES[host]="%B%F{yellow}"
 MEGAPROMPT_STYLES[userhost_brackets]="%b%F{green}"
 MEGAPROMPT_STYLES[username]="%B%F{green}"
+MEGAPROMPT_STYLES[username_root]="%B%F{red}"
 MEGAPROMPT_STYLES[at]="%b%F{green}"
 MEGAPROMPT_STYLES[dir_owner]="%B%F{blue}"
 MEGAPROMPT_STYLES[dir_group]="%B%F{green}"
 MEGAPROMPT_STYLES[dir_nowrite]="%B%F{red}"
 MEGAPROMPT_STYLES[dir_write]="%B%F{yellow}"
 MEGAPROMPT_STYLES[histnum]="%b%F{blue}"
-MEGAPROMPT_STYLES[dollar]="%b%F{default}"
+MEGAPROMPT_STYLES[prompt]="%b%F{default}"
+MEGAPROMPT_STYLES[prompt_char]="λ"
 MEGAPROMPT_STYLES[git_master]="%b%F{white}"
 MEGAPROMPT_STYLES[git_other]="%b%F{red}"
+MEGAPROMPT_STYLES[git_branch_brackets]="%b%F{grey}"
+MEGAPROMPT_STYLES[hg_branch_brackets]="%b%F{grey}"
 MEGAPROMPT_STYLES[jobs]="%B%F{magenta}"
 MEGAPROMPT_STYLES[jobs_brackets]="%b%F{red}"
 typeset -Ag MEGAPROMPT_KEYMAP_IND
@@ -29,49 +33,64 @@ MEGAPROMPT_KEYMAP_IND[keymap_unlisted]="%b%K{white}%F{black}?%k"
 PS1_cmd_stat='%(?,, %b%F{cyan}<%F{red}%?%F{cyan}>)'
 PS1_jobs='%(1j, ${MEGAPROMPT_STYLES[jobs_brackets]}[${MEGAPROMPT_STYLES[jobs]}%jj${MEGAPROMPT_STYLES[jobs_brackets]}],)'
 
---getHgBranchForPrompt() {
+mp-getHgBranch() {
     local branch
     branch=$(hg branch 2>/dev/null)
-    if [[ "$branch" = "default" ]]; then
-        echo "%F{grey}<${MEGAPROMPT_STYLES[git_master]}${branch}%F{grey}> "
-    elif [[ -n "$branch" ]]; then
-        echo "%F{grey}<${MEGAPROMPT_STYLES[git_other]}${branch}%F{grey}> "
+    if [[ -z "$branch" ]]; then
+        return
     fi
+    echo -n "${MEGAPROMPT_STYLES[hg_branch_brackets]}<"
+    mp-styleBranch "$branch" hg
+    echo -n "${MEGAPROMPT_STYLES[hg_branch_brackets]}> "
 }
 
---getGitBranchForPrompt() {
+mp-getGitBranch() {
     local branch
     branch=$(git branch 2>/dev/null | fgrep '*')
-    if [ "$branch" = "* master" ]
-    then
-        branch="%F{grey}[${MEGAPROMPT_STYLES[git_master]}${branch:2}%F{grey}] "
-    elif [ -n "$branch" ]
-    then
-        branch="%F{grey}[${MEGAPROMPT_STYLES[git_other]}${branch:2}%F{grey}] "
+    if [[ -z "$branch" ]]; then
+        return
     fi
-    echo $branch
+    branch=${branch:2}
+    echo -n "${MEGAPROMPT_STYLES[git_branch_brackets]}["
+    mp-styleBranch "$branch" git
+    echo -n "${MEGAPROMPT_STYLES[git_branch_brackets]}] "
 }
 
---getPwdForPrompt() {
+mp-styleBranch() {
+    local branch
+    local cvs
+    branch=$1
+    cvs="$2"
+    if [ "$branch" = "master" ]
+    then
+        branch="${MEGAPROMPT_STYLES[git_master]}$branch"
+    elif [ -n "$branch" ]
+    then
+        branch="${MEGAPROMPT_STYLES[git_other]}$branch"
+    fi
+    echo -n $branch
+}
+
+-mp-getPwd() {
     local out
     local dir
     dir="$PWD"
     out=""
     while true; do
         if [[ "$dir" = "$HOME" ]]; then
-            echo "$(--getDirColor $dir)~/${out}"
+            echo "$(-mp-getDirColor $dir)~/${out}"
             return
         elif [[ "$dir" = "/" ]]; then
-            echo "$(--getDirColor $dir)/${out}"
+            echo "$(-mp-getDirColor $dir)/${out}"
             return
         else
-            out="$(--getDirColor $dir)$(basename $dir)/${out}"
+            out="$(-mp-getDirColor $dir)$(basename $dir)/${out}"
             dir="$(dirname $dir)"
         fi
     done
 }
 
---getDirColor() {
+-mp-getDirColor() {
     # I want to know if I'm the owner, and if it's writeable
     local dir
     dir="$1"
@@ -94,7 +113,15 @@ PS1_jobs='%(1j, ${MEGAPROMPT_STYLES[jobs_brackets]}[${MEGAPROMPT_STYLES[jobs]}%j
     fi
 }
 
-update-prompt() {
+mp-getUser(){
+    if [[ "$USER" = "root" ]]; then
+        echo "${MEGAPROMPT_STYLES[username_root]}$USER"
+    else
+        echo "${MEGAPROMPT_STYLES[username]}$USER"
+    fi
+}
+
+mp-updatePrompt() {
     local -A s
     set -A s ${(kv)MEGAPROMPT_STYLES}
     local k=$MEGAPROMPT_KEYMAP_IND[$ZSH_CUR_KEYMAP]
@@ -102,16 +129,17 @@ update-prompt() {
         k=$MEGAPROMPT_KEYMAP_IND[keymap_unlisted]
     fi
 
-    PS1="$s[time]%T ${s[userhost_brackets]}[${s[username]}%n${s[at]}@${s[host]}%m${s[userhost_brackets]}] \$(--getGitBranchForPrompt)\$(--getHgBranchForPrompt)\$(--getPwdForPrompt)${PS1_jobs}${PS1_cmd_stat}
-${k} ${s[histnum]}%h ${s[dollar]}%# "
+    PS1="$s[time]%T ${s[userhost_brackets]}[\$(mp-getUser)${s[at]}@${s[host]}%m${s[userhost_brackets]}] \$(mp-getGitBranch)\$(mp-getHgBranch)\$(-mp-getPwd)${PS1_jobs}${PS1_cmd_stat}
+${k} ${s[histnum]}%h ${s[prompt]}${s[prompt_char]} "
     if zle; then
         zle reset-prompt
     fi
 }
 
 if type -f hooks-add-hook 1>/dev/null 2>&1; then
-    hooks-add-hook zle_keymap_select_hook update-prompt
-    add-zsh-hook precmd update-prompt
+    hooks-add-hook zle_keymap_select_hook mp-updatePrompt
+    hooks-add-hook zle_line_init_hook mp-updatePrompt
+    add-zsh-hook precmd mp-updatePrompt
 else
     echo "zsh-hooks not loaded!  Please load willghatch/zsh-hooks before zsh-megaprompt." 1>&2
 fi
